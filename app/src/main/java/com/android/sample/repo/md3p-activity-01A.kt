cb499385cf6f8cffb7098dc1348777e6dcbba8fa
Novenreygumnad <novenreygumnad@gmail.com>
2023-02-09 21:13:13 +0800

    change mainactivity1's name to mad3p-activity-01a and add a list of students plus break command.

--- app/src/main/java/com/android/sample/repo/md3p-activity-01A.kt
+++ app/src/main/java/com/android/sample/repo/md3p-activity-01A.kt
@@ -1,10 +1,26 @@
 package com.android.sample.repo
 
-// Name of students and their grades(average)
-fun main(args: Array<String>) {
-    val welcome: String = "MD3P STUDENTS RATING"
-    println(welcome)
+// Name of students and their ratings
 
+fun main(args: Array<String>) {
+    val welcome: String = "MD3P STUDENT RATING"
+    val nameOfStudents: String = "List Of Students"
+    println(welcome)
+    println(nameOfStudents)
+
+/*ginamit ko po  sa line 15-23 ang listOf function to create a list of strings
+ with the names and then loops through each name using
+ forEach and prints it to the console.
+ */
+    val names = listOf("Andrew Bustos", "Faith Mariano", "Felix Navarro", "Gee Quero", "Jaramillo Hanz",
+        "Jeffrey Laroya", "Jeje Elenzano", "Joe Mari Ubay", "John Chris Macaraig Bolanio",
+        "Jun Zon", "KI NG KL Lopez", "Kram Krad", "Lander Pelagio", "Ley De",
+        "Marie Fe Dela Peña", "Paolo Luzares", "Raii Ichinose", "Riz Caser", "Seong Phl",
+        "Vanchan Navidad", "Vic Ama", "Xandrei Xhou", "Yer Bravo Toilas")
+
+    names.forEach { name ->
+        println(name)
+    }
     val studentDirectory = mutableMapOf<String, Student>()
     val studentList = arrayOf("Andrew Bustos", "Faith Mariano", "Felix Navarro", "Gee Quero", "Jaramillo Hanz",
         "Jeffrey Laroya", "Jeje Elenzano", "Joe Mari Ubay", "John Chris Macaraig Bolanio", "Jun Zon", "KI NG KL Lopez",
@@ -18,7 +34,11 @@ fun main(args: Array<String>) {
 
     while (true) {
         println("Please enter student name: ")
-        val studentName = readLine() ?: break
+        val studentName = readLine()
+        if (studentName.isNullOrBlank()) {
+            break
+        }
+
         val student = studentDirectory[studentName]
         if (student == null) {
             println("Student not found.")
@@ -27,19 +47,23 @@ fun main(args: Array<String>) {
 
         println("Enter student rating (pass/fail): ")
         val rating = readLine()
+        if (rating.isNullOrBlank()) {
+            continue
+        }
         if (rating == "pass") {
             student.pass()
+            break
         } else if (rating == "fail") {
             student.fail()
+            break
         } else {
             println("Invalid rating.")
             continue
         }
-        println("Student $studentName rating updated to $rating.")
-        println("Current student ratings:")
-        for ((name, student) in studentDirectory) {
-            println("$name rating: ${if (student.passed) "pass" else "fail"}")
-        }
+    }
+    println("Current student ratings:")
+    for ((name, student) in studentDirectory) {
+        println("$name rating: ${if (student.passed) "pass" else "fail"}")
     }
 }
 
